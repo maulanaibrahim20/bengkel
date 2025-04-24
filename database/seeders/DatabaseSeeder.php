d56e837177fb3d40765884c3b778f914b02829fc
maulanaibrahim20 <maulanaibrahim20@student.polindra.ac.id>
2025-04-24 19:40:46 +0700

    feat : update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\CmsComponent;
-use App\Models\CmsPages;
 use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
